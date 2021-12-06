63b3b4c1b81fd55c990d956eaab210bd75b5ecfe
Nick <zzz727272@gmail.com>
2021-12-06 15:24:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,110 @@
+"use strict";
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+
+console.log(name);
+var name = 'Nick';
+
+{
+    var result = 50;
+}
+
+console.log(result); */
+
+/* alert('Hello');
+
+const result = confirm("Are you here?");
+console.log(result);
+
+const answer = +prompt("Вам есть 18?", "18");
+console.log(answer +5); */
+
+/* const answers = []; */
+
+/* answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', ''); */
+
+/* document.write(answers); */
+
+/* console.log(typeof(answers));
+console.log(typeof(null)); */
+
+/* const category = 'toys'; */
+
+/* console.log('https://someurl.com/' + category); */
+
+/* console.log(`https://someurl.com/${category}/5`);
+
+const user = "Nick";
+
+alert(`Привет, ${user}`); */
+
+/* console.log('arr' + "- object");
+console.log(4 + +"5");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr); */
+
+let incr = 10,
+    decr = 10;
+
+/* ++incr;
+--decr; */
+
+/* console.log(incr++);
+console.log(decr--); */
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+console.log(2*4 == '8');
+
+console.log(2*4 === '8');
+
+console.log(2*4 === 8);
+
+const isChecked = true,
+      isClose = false;
+
+/* console.log(isChecked && isClose); */
+
+/* console.log(isChecked || isClose); */
+
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 === 8);
+
+console.log(2 + 2 * 2 != 8);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+
+
+
+
